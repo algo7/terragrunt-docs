54268571a234eaad7f3f792090cea0d755d9689f
algo7 <11154774+algo7@users.noreply.github.com>
2023-06-30 23:24:10 +0200

    parsing poc

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/hcl2/gohcl"
 	"github.com/hashicorp/hcl2/hclparse"
 )
 
+type foo struct {
+	A string   `hcl:"a, omitempty"`
+	B []string `hcl:"b" omitempty"`
+}
+
 func main() {
 	parser := hclparse.NewParser()
+
+	f, parseDiags := parser.ParseHCLFile("test.hcl")
+
+	if parseDiags.HasErrors() {
+		fmt.Println(parseDiags.Error())
+	}
+
+	var fooInstance foo
+	decodeDiags := gohcl.DecodeBody(f.Body, nil, &fooInstance)
+	if decodeDiags.HasErrors() {
+		log.Fatal(decodeDiags.Error())
+	}
+
+	fmt.Printf("%#v", fooInstance)
 }
